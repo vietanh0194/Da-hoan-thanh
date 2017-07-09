b587a9781c839bcb7a377954031144c07615aaca
Viet Anh <vietanh@MacBook-Pro.net.fpt>
2017-07-09 21:14:01 +0700

    demo mau

---
+++ demoAutoLayout1/demoAutoLayout1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  demoAutoLayout1
+//
+//  Created by Viet Anh on 3/9/17.
+//  Copyright © 2017 Viet Anh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
